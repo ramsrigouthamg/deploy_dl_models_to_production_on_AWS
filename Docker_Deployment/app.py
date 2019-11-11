5d91470a8374068765438a75623340e8c875f20d
ramsrigouthamg <ramsrigouthamg@gmail.com>
2019-11-11 19:20:33 +0800

    Update

--- Docker_Deployment/app.py
+++ Docker_Deployment/app.py
@@ -48,4 +48,4 @@ def get_entities():
 
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", debug=False)
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=False, port=80)
